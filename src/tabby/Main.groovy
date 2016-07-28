98e552b9cd106e4b4739377e2da37e9e8a3f0f40
susannej <susanne.jaeckel@lucke-edv.de>
2016-07-28 18:26:19 +0200

    ...

--- src/tabby/Main.groovy
+++ src/tabby/Main.groovy
@@ -61,14 +61,6 @@ start {
         fxaction(id: 'effectSlide', icon: 'resources/icons/effect_slide.png', onAction: {})
         fxaction(id: 'effectStaccato', icon: 'resources/icons/effect_staccato.png', onAction: {})
 
-        /*
-        fxaction(id: 'zoom25', name: '25%', onAction: { songCanvas.resize(2000 * 0.25, 3000 * 0.25) })
-        fxaction(id: 'zoom50', name: '50%', onAction: { songCanvas.resize(2000 * 0.5, 3000 * 0.5) })
-        fxaction(id: 'zoom75', name: '75%', onAction: { songCanvas.resize(2000 * 0.75, 3000 * 0.75) })
-        fxaction(id: 'zoom100', name: '100%', onAction: { songCanvas.resize(2000, 3000) })
-        fxaction(id: 'zoom125', name: '125%', onAction: { songCanvas.resize(2000 * 1.25, 3000 * 1.25) })
-        fxaction(id: 'zoom150', name: '150%', onAction: { songCanvas.resize(2000 * 1.5, 3000 * 1.5) })
-        */
         fxaction(id: 'zoom25', name: '25%', onAction: { songCanvas.zoom(0.25) })
         fxaction(id: 'zoom50', name: '50%', onAction: { songCanvas.zoom(0.5) })
         fxaction(id: 'zoom75', name: '75%', onAction: { songCanvas.zoom(0.75) })
@@ -77,7 +69,7 @@ start {
         fxaction(id: 'zoom150', name: '150%', onAction: { songCanvas.zoom(1.5) })
     }
 
-    stage(title: "Tabby ${config.version}", width: 800, height: 450, visible: true) {
+    stage(title: "Tabby ${config.version}", width: 1024, height: 768, visible: true) {
         scene(stylesheets: 'theme/dark.css', fill: GROOVYBLUE) {
             borderPane {
                 top {
@@ -148,7 +140,7 @@ start {
                 }
                 center {
                     scrollPane {
-                        songCanvas(id: 'songCanvas', width: 2200, height: 3070, borderSize: 50)
+                        songCanvas(id: 'songCanvas', paperSize: 'A4',/*width: 2200, height: 3070,*/ borderSize: 50)
                     }
                 }
             }
--- src/tabby/song/SongCanvas.groovy
+++ src/tabby/song/SongCanvas.groovy
@@ -10,14 +10,30 @@ import javafx.scene.paint.Color
 class SongCanvas extends ResizableCanvas {
 
     @FXBindable
-    Double initWidth
+    Double initWidth    // width of the paper without border
     @FXBindable
-    Double initHeight
+    Double initHeight   // height of the paper without border
     @FXBindable
     Double borderSize
+    @FXBindable
+    String paperSize    // e.g. 'a4', 'letter' etc.
+    @FXBindable
+    Double zoom = 1.0   // current zoom-ratio
 
-    void zoom(double zoom) {
-        resize(initWidth * zoom, initHeight * zoom)
+    static def paperSizes = [
+            'a4'    : [w: 2100d, h: 2970d],
+            'a5'    : [w: 1480d, h: 2100d],
+            'letter': [w: 2100d, h: 2794d],
+            'legal' : [w: 2100d, h: 3556d]
+    ]
+
+    void zoom(double newZoom) {
+        zoom = newZoom
+        zoomDraw()
+    }
+
+    void zoomDraw() {
+        resize((initWidth + 2 * borderSize) * zoom, (initHeight + 2 * borderSize) * zoom)
         graphicsContext2D.scale(zoom, zoom)
         draw()
         double restoreZoom = 1 / zoom
@@ -27,22 +43,44 @@ class SongCanvas extends ResizableCanvas {
     @Override
     void draw() {
         GraphicsContext gc = graphicsContext2D
-        gc.clearRect(0, 0, initWidth, initHeight)
+        gc.clearRect(0, 0, initWidth + 2 * borderSize, initHeight + 2 * borderSize)
 
         gc.fill = Color.WHITE
-        gc.fillRect(borderSize, borderSize, initWidth - 2 * borderSize, initHeight - 2 * borderSize)
+        gc.fillRect(borderSize, borderSize, initWidth, initHeight)
 
         gc.stroke = Color.RED
-        gc.strokeLine(borderSize, borderSize, initWidth - borderSize, initHeight - borderSize)
-        gc.strokeLine(borderSize, initHeight - borderSize, initWidth - borderSize, borderSize)
+        gc.strokeLine(borderSize, borderSize, initWidth + borderSize, initHeight + borderSize)
+        gc.strokeLine(borderSize, initHeight + borderSize, initWidth + borderSize, borderSize)
     }
 
     @Override
     void resize(double nwidth, double nheight) {
         setWidth(nwidth)
         setHeight(nheight)
-        if (initWidth == 0.0d) setInitWidth(nwidth)
-        if (initHeight == 0.0d) setInitHeight(nheight)
+    }
+
+    void setInitHeight(double nheight) {
+        initHeightProperty().set(nheight)
+        setHeight(initHeight + 2 * borderSize)
+    }
+
+    void setInitWidth(double nwidth) {
+        initWidthProperty().set(nwidth)
+        setWidth(initWidth + 2 * borderSize)
+    }
+
+    void setBorderSize(double bSize) {
+        borderSizeProperty().set(bSize)
+        setWidth(initWidth + 2 * borderSize)
+        setHeight(initHeight + 2 * borderSize)
+        zoomDraw()
+    }
+
+    void setPaperSize(String pSize) {
+        paperSizeProperty().set(pSize)
+        initWidth = paperSizes[pSize.toLowerCase()]['w']
+        initHeight = paperSizes[pSize.toLowerCase()]['h']
+        zoomDraw()
     }
 
 }
